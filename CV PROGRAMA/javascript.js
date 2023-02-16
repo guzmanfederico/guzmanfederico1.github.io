f5865c74afe0d5a25e5e04770e81a1cd8ccfb9a4
guzmanfederico <123780427+guzmanfederico@users.noreply.github.com>
2023-02-16 01:03:51 -0300

    Add files via upload

---
+++ CV PROGRAMA/javascript.js
@@ -0,0 +1,4 @@
+document.getElementById("boton").addEventListener("click", function () {
+    console.log("hola mundo desde mi cv");
+    document.getElementById("dato").innerHTML = "Edad 21 años - Argentino - Vegano";
+})
